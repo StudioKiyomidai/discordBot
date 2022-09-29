485e1716c1fb216d56f5c198832d18ccc4e74b95
SK-Komori <ksg.18b07@gmail.com>
2022-09-29 13:33:07 +0900

    index.js変更

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 const http = require("http");
 const axios = require('axios');
-const { Client, GatewayIntentBits, SlashCommandBuilder } = require('discord.js'); //discord.js からClientとIntentsを読み込む
+const { Client, GatewayIntentBits } = require('discord.js'); //discord.js からClientとIntentsを読み込む
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildMessages] });  //clientインスタンスを作成する
 
 // 起動時実行関数
 client.once('ready', async () => {
-    const data = new SlashCommandBuilder()
-        .setName("ping")
-        .setDescription("Replies with Pong!")
-        .addStringOption(option =>
-            option
-                .setName('text')
-                .setDescription('何か入力して')
-        );
+    const data = [{
+        name: "ping",
+        description: "Replies with Pong!",
+        options: [
+            {
+                type: 3, // string
+                name: "text",
+                description: "何か入力してみて",
+                required: true,
+            }
+        ]
+    }];
     await client.application.commands.set(data);
     console.log('起動完了'); //黒い画面(コンソール)に「起動完了」と表示させる
 });
@@ -24,7 +28,7 @@ client.login(process.env.DISCORD_BOT_TOKEN);
 const commands = {
     async ping(interaction) {
         const jsonData = {
-            message: interaction.options.getString("text")
+            message: interaction.options.get("text")
         }
         try {
             await axios({
@@ -51,26 +55,7 @@ client.on("interactionCreate", async (interaction) => {
     if (!interaction.isCommand()) {
         return;
     }
-    if (interaction.commandName === 'ping') {
-        const jsonData = {
-            message: interaction.options.getString("text")
-        }
-        try {
-            await axios({
-                method: "post",
-                url: process.env.GAS_URL,
-                data: jsonData,
-                responseType: "json",
-            }).then((response) => {
-                console.log("status: " + response.status)
-                console.log(response.data)
-                interaction.reply("seikou shiteru wayo");
-            });
-        } catch (error) {
-            console.log(error)
-            interaction.reply('error shiteru na')
-        }
-    }
+    return commands[interaction.commandName](interaction);
 });
 
 // GASからのリクエスト受付
