8819b02d858e024cca690be090a17e974ae0afd0
SK-Komori <ksg.18b07@gmail.com>
2022-09-29 09:34:33 +0900

    index.js変更

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { Client, GatewayIntentBits } = require('discord.js'); //discord.js からClientとIntentsを読み込む
 
-const client = new Client({ intents: [GatewayIntentBits.GUILDS, GatewayIntentBits.GUILD_MEMBERS, GatewayIntentBits.GUILD_MESSAGES] });  //clientインスタンスを作成する
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildMessages] });  //clientインスタンスを作成する
 
 client.once('ready', () => { //ここにボットが起動した際のコードを書く(一度のみ実行)
     console.log('起動完了'); //黒い画面(コンソール)に「起動完了」と表示させる
