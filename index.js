99ac8a3c1be5f28669d93d5051645302aaa9c6f9
SK-Komori <ksg.18b07@gmail.com>
2022-09-29 09:16:33 +0900

    index.js変更

--- index.js
+++ index.js
@@ -1,71 +1,8 @@
-//モジュールのインポート
-const http = require("http");
-const axios = require('axios');
-const discord = require("discord.js");
-let client;
+const { Client, Intents } = require('discord.js'); //discord.js からClientとIntentsを読み込む
 
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGES] });  //clientインスタンスを作成する
 
-const connectDiscord = () => {
-    client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS] });
-    client.login(process.env.DISCORD_BOT_TOKEN);
-    client.on("ready", () => console.log("bot is ready!"));
-
-    //送信者がbot自身でないとき、GASへPOSTする関数を実行
-    client.on("messageCreate", (message) => !message.author.bot ? sendGAS(message) : "");
-
-    if (process.env.DISCORD_BOT_TOKEN == undefined) {
-        console.log("please set ENV: DISCORD_BOT_TOKEN");
-        process.exit(0);
-    }
-};
-
-
-// GASにデータをPOSTする関数
-const sendGAS = (message) => {
-    const jsonData = {
-        author: message.author,
-        content: message.content,
-        channel: message.channel,
-    };
-    const post = async () => {
-        try {
-            await axios({
-                method: "post",
-                url: process.env.GAS_URL,
-                data: jsonData,
-                responseType: "json",
-            }).then((response) => {
-                const msg = response.data;
-
-                //送信方法を振り分け
-                switch (msg.messagetype) {
-                    case "nothing": //何もしない
-                        break;
-
-                    case "reply": //返信
-                        message.reply(msg.content);
-                        break;
-
-                    case "send": //ただ送る
-                    default:
-                        message.channel.send(msg.content);
-                        break;
-                }
-            });
-        } catch (error) {
-            message.reply("an error occurred: " + error);
-        }
-    };
-    post();
-};
-
-//実行
-connectDiscord();
-
-//GASからのPOSTリクエストを受け取る用
-http
-    .createServer((request, response) => {
-        console.log('post from gas')
-        response.end("Discord bot is active now.");
-    })
-    .listen(3000);
+client.once('ready', () => { //ここにボットが起動した際のコードを書く(一度のみ実行)
+    console.log('起動完了'); //黒い画面(コンソール)に「起動完了」と表示させる
+});
+client.login(process.env.DISCORD_BOT_TOKEN);
