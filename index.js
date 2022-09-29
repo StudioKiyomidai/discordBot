5c74aa43eec525ba04e5257a152e77ec43cdc210
SK-Komori <ksg.18b07@gmail.com>
2022-09-29 10:46:50 +0900

    slash command追加

--- index.js
+++ index.js
@@ -2,7 +2,21 @@ const { Client, GatewayIntentBits } = require('discord.js'); //discord.js から
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildMessages] });  //clientインスタンスを作成する
 
-client.once('ready', () => { //ここにボットが起動した際のコードを書く(一度のみ実行)
+client.once('ready', async () => { //ここにボットが起動した際のコードを書く(一度のみ実行)
+    const data = [{
+        name: "ping",
+        description: "Replies with Pong!",
+    }];
+    await client.application.commands.set(data, process.env.GUILD_ID);
     console.log('起動完了'); //黒い画面(コンソール)に「起動完了」と表示させる
 });
 client.login(process.env.DISCORD_BOT_TOKEN);
+
+client.on("interactionCreate", async (interaction) => {
+    if (!interaction.isCommand()) {
+        return;
+    }
+    if (interaction.commandName === 'ping') {
+        await interaction.reply('Pong！');
+    }
+});
